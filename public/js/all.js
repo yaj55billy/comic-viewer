21a7957d8e2af415af0e6d481afd6c4f59e36a4e
紀冠逸 <yaj55ushydna@gmail.com>
2018-07-08 22:08:56 +0800

    v

---
+++ public/js/all.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var newFunction = function newFunction() {
+    console.log('a');
+};
+
+newFunction();
+'use strict';
+
+var nw2 = function newFunction() {
+    console.log('b');
+};
+nw2();
+//# sourceMappingURL=all.js.map
